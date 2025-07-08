cbdf7ed85d73233862c2008241fedfcb653010f3
GitHub Action <action@github.com>
2025-07-08 23:38:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/kirillmaiboroda1996/pydanticsmartdocs/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/kirillmaiboroda1996/pydanticsmartdocs/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.kirillmaiboroda1996.pydanticsmartdocs.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
